edceecd5559390d1040b3c3f45f74bb23cca72b5
PERAPAT <137144072+Perapats@users.noreply.github.com>
2025-09-10 02:27:16 +0700

    Delete Human Relations directory

--- Human Relations/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", () => {
-    alert("✅ Thank you! Your response has been recorded.");
-    form.reset();
-  });
-});
